ba4739c988aef7410df3c2216ac2c28e7310f41f
Benjamin Garcia <benjg208@gmail.com>
2025-12-22 02:51:56 -0600

    Add comments to periodic method in RobotContainer to clarify call order

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/RobotContainer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/RobotContainer.java
@@ -19,6 +19,16 @@ public class RobotContainer {
     }
 
     public void periodic() {
+        /*
+         * Call periodic methods of all subsystems
+         *
+         * Order of calls:
+         * 1 - Vision to get latest data
+         * 2 - Drive to update pose
+         * 3 - Shooter to adjust to new target if needed
+         * 4 - Intake to respond to driver inputs
+         * 5 - LED to update status
+         */
         robot.vision.periodic();
         robot.drive.periodic();
         robot.shooter.periodic();
