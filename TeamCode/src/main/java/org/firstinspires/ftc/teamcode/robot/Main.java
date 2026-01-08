30d19b7679eae0418f4b4d1f3b88e91f09fff9c9
Benjamin Garcia <benjg208@gmail.com>
2026-01-08 14:16:33 -0600

    Implement defensive maneuvers by adding DDOS and firing squad actions in Main loop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Main.java
@@ -17,6 +17,10 @@ public class Main extends LinearOpMode {
         while (opModeIsActive()) {
             container.periodic(telemetry);
             telemetry.update();
+
+            container.ddosOtherRobots();
+            container.deployRazorBlades();
+            container.firingSquad(container.getOpposingRobots());
         }
 
         // Stop all subsystems
