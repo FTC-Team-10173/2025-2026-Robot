561a4c2529b68245d1e26a7e8b7bba7bef2137d0
Bnenne <benjg208@gmail.com>
2026-01-19 12:17:00 -0600

    Add PoseEstimator class for improved pose localization using multiple localizers, very basic as of now will continue working on

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/PoseEstimator.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.Roadrunner.Localizer;
+import org.firstinspires.ftc.teamcode.Roadrunner.OTOSLocalizer;
+import org.firstinspires.ftc.teamcode.Roadrunner.ThreeDeadWheelLocalizer;
+
+public class PoseEstimator implements Localizer {
+
+    private final ThreeDeadWheelLocalizer tdwLocalizer;
+    private final OTOSLocalizer otosLocalizer;
+
+    public PoseEstimator(HardwareMap hardwareMap, double inPerTick, Pose2d pose) {
+        tdwLocalizer = new ThreeDeadWheelLocalizer(hardwareMap, inPerTick, pose);
+        otosLocalizer = new OTOSLocalizer(hardwareMap, pose);
+    }
+
+    @Override
+    public void setPose(Pose2d pose) {
+        tdwLocalizer.setPose(pose);
+        otosLocalizer.setPose(pose);
+    }
+
+    @Override
+    public Pose2d getPose() {
+        Pose2d tdwPose = tdwLocalizer.getPose();
+        Pose2d otosPose = otosLocalizer.getPose();
+
+        // Simple average of the two poses
+        double x = (tdwPose.position.x + otosPose.position.x) / 2.0;
+        double y = (tdwPose.position.y + otosPose.position.y) / 2.0;
+        double heading = (tdwPose.heading.toDouble() + otosPose.heading.toDouble()) / 2.0;
+
+        return new Pose2d(x, y, heading);
+    }
+
+    @Override
+    public PoseVelocity2d update() {
+        PoseVelocity2d tdwVelPose = tdwLocalizer.update();
+        PoseVelocity2d otosVelPose = otosLocalizer.update();
+
+        // Simple average of the two poses
+        double x = (tdwVelPose.linearVel.x + tdwVelPose.linearVel.x) / 2.0;
+        double y = (tdwVelPose.linearVel.y + tdwVelPose.linearVel.y) / 2.0;
+        double angVel = (otosVelPose.angVel + otosVelPose.angVel) / 2.0;
+
+        return new PoseVelocity2d(
+                new Vector2d(x, y),
+                angVel
+        );
+    }
+}
