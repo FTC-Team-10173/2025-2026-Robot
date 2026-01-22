20394891e6b927650b9059b9d9b1ac7b826853cc
Bnenne <benjg208@gmail.com>
2026-01-22 07:25:24 -0600

    Create default method in Localizer for pose estimation with Limelight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Roadrunner/Localizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Roadrunner/Localizer.java
@@ -2,6 +2,9 @@ package org.firstinspires.ftc.teamcode.Roadrunner;
 
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.arcrobotics.ftclib.geometry.Translation2d;
+
+import org.firstinspires.ftc.teamcode.robot.subsystems.Limelight;
 
 /**
  * Interface for localization methods.
@@ -22,4 +25,19 @@ public interface Localizer {
      * @return the Localizer's current velocity estimate
      */
     PoseVelocity2d update();
+
+    default void estimateLL(Limelight.Botpose botpose) {
+        Pose2d currentPose = getPose();
+
+        Translation2d newPose = new Translation2d(
+                (botpose.x * 0.1 * 39.37) + (currentPose.position.x * 0.9),
+                (botpose.y * 0.1 * 39.37) + (currentPose.position.y * 0.9)
+        );
+
+        setPose(new Pose2d(
+                newPose.getX(),
+                newPose.getY(),
+                currentPose.heading.toDouble()
+        ));
+    }
 }
